ed3c413bd44b690b49b09f52d52c6891638c5fe9
madhank93 <maddymadhan946@gmail.com>
2021-09-11 18:11:35 +0530

    fix: reordering coredns and increasing max limit of selenosis

--- selenosis-deploy/script.sh
+++ selenosis-deploy/script.sh
@@ -2,6 +2,8 @@
 
 kubectl apply -f ./selenosis-deploy/01-namespace.yaml
 
+kubectl apply -f ./selenosis-deploy/06-coredns.yaml
+
 if [ $LOCAL ] 
 then
     kubectl create cm selenosis-config --from-file=browsers.yaml=selenosis-deploy/vnc-browsers.yaml -n selenosis
@@ -15,5 +17,3 @@ kubectl apply -f ./selenosis-deploy/02-service.yaml
 kubectl apply -f ./selenosis-deploy/03-selenosis.yaml
 
 kubectl apply -f ./selenosis-deploy/05-selenosis-hpa.yaml
-
-kubectl apply -f ./selenosis-deploy/06-coredns.yaml
