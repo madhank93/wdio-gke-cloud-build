8933967fa5154952c6d17838ae43802116ccb459
madhank93 <maddymadhan946@gmail.com>
2021-09-13 17:16:37 +0530

    feat: adding stern for logging

--- selenosis-deploy/script.sh
+++ selenosis-deploy/script.sh
@@ -4,6 +4,17 @@ kubectl apply -f ./selenosis-deploy/01-namespace.yaml
 
 kubectl apply -f ./selenosis-deploy/06-coredns.yaml
 
+wget https://github.com/stern/stern/releases/download/v1.20.1/stern_1.20.1_linux_amd64.tar.gz
+
+tar xvzf stern_1.20.1_linux_amd64.tar.gz
+
+mv stern_1.20.1_linux_amd64 stern
+
+chmod +x stern
+
+./stern -n selenosis --selector='type=browser' > seleniferous.log
+./stern -n selenosis --selector='app=selenosis' > selenosis.log
+
 if [ $LOCAL ] 
 then
     kubectl create cm selenosis-config --from-file=browsers.yaml=selenosis-deploy/vnc-browsers.yaml -n selenosis
