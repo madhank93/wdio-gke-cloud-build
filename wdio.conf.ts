91b3a5dea2e9178bbb36dd82825ee1f5a59890be
madhank93 <maddymadhan946@gmail.com>
2021-09-15 09:31:57 +0530

    fix: increasing wait timeout and removing pre pull job from selenosis namespace

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -105,10 +105,10 @@ export const config: WebdriverIO.Config = {
   //
   // Default timeout in milliseconds for request
   // if Selenium Grid doesn't send response
-  connectionRetryTimeout: 180000,
+  connectionRetryTimeout: 210000,
   //
   // Default request retries count
-  connectionRetryCount: 3,
+  connectionRetryCount: 5,
   //
   // Test runner services
   // Services take over a specific job you don't want to take care of. They enhance
